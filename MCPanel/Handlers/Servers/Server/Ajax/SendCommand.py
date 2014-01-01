9ceb057af698ddae2d6424e5a1db18b9d05ccf0e
brayden <brayden.hull@gmail.com>
2014-01-01 19:30:08 +0800

    Fixing up capitalisation error part 2

---
+++ MCPanel/Handlers/Servers/Server/Ajax/SendCommand.py
@@ -0,0 +1,15 @@
+__author__ = 'brayden'
+
+from tornado.web import asynchronous
+from tornado.web import authenticated
+from Base import BaseServerAjaxHandler
+
+
+class SendCommandHandler(BaseServerAjaxHandler):
+    @asynchronous
+    @authenticated
+    def post(self, server_id):
+        if 'command' in self.request.arguments:
+            self.finish({"result": {"message": None, "success": self.application.supervisor.run_command('%s%s' % (self.application.process_prefix, server_id), self.get_argument('command'))}})
+        else:
+            self.finish({"result": {"success": False, "message": "Insufficient arguments"}})
