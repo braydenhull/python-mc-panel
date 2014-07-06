e9f8ea83bf756e441a014a22b4e679e25050eb36
brayden <brayden.hull@gmail.com>
2014-07-06 22:33:02 +0800

    Added support for new JSON format for ops list and ban list

--- MCPanel/Handlers/Servers/Server/Ajax/GetBannedPlayers.py
+++ MCPanel/Handlers/Servers/Server/Ajax/GetBannedPlayers.py
@@ -3,37 +3,49 @@ __author__ = 'brayden'
 from tornado.web import asynchronous
 from tornado.web import authenticated
 from Base import BaseServerAjaxHandler
+import os
+import json
 
 
 class GetBannedPlayersHandler(BaseServerAjaxHandler):
     @asynchronous
     @authenticated
     def post(self, server_id):
-        with open(self.application.config.get('minecraft', 'home') + '/%s%s/banned-players.txt' % (self.application.process_prefix, server_id), 'r') as f:
-            lines = list(line for line in (l.strip() for l in f) if line) # this mess provided by http://stackoverflow.com/a/4842095/2077881
+        if os.path.exists(self.application.config.get('minecraft', 'home') + '/%s%s/banned-players.json' % (self.application.process_prefix, server_id)):
+            with open(self.application.config.get('minecraft', 'home') + '/%s%s/banned-players.json' % (self.application.process_prefix, server_id), 'r') as f:
+                data = json.load(f)
 
-        result = {"players": [], "details": {}}
+            result = {"players": [], "details": {}}
 
-        for line in lines:
-            if line.startswith('#'):
-                continue
+            for player in data:
+                result['players'].append(player['name'])
+                result['details'][player['name']] = {"Ban Date": player['created'], "Banned By": player['source'], "Banned Until": player['expires'], "Reason": player['reason']}
+        else:
+            with open(self.application.config.get('minecraft', 'home') + '/%s%s/banned-players.txt' % (self.application.process_prefix, server_id), 'r') as f:
+                lines = list(line for line in (l.strip() for l in f) if line) # this mess provided by http://stackoverflow.com/a/4842095/2077881
 
-            player = line.split('|')
-            result['players'].append(player[0])
+            result = {"players": [], "details": {}}
 
-            if len(player) > 1:  # back in my day banned-players.txt was just a list of names
-                result['details'][player[0]] = {
-                    "Ban Date": player[1],
-                    "Banned By": player[2],
-                    "Banned Until": player[3],
-                    "Reason": player[4]
-                }
-            else:
-                result['details'][player[0]] = {
-                    "Ban Date": "Not Available",
-                    "Banned By": "Unknown",
-                    "Banned Until": "Forever",
-                    "Reason": "Unknown"
-                }
+            for line in lines:
+                if line.startswith('#'):
+                    continue
+
+                player = line.split('|')
+                result['players'].append(player[0])
+
+                if len(player) > 1:  # back in my day banned-players.txt was just a list of names
+                    result['details'][player[0]] = {
+                        "Ban Date": player[1],
+                        "Banned By": player[2],
+                        "Banned Until": player[3],
+                        "Reason": player[4]
+                    }
+                else:
+                    result['details'][player[0]] = {
+                        "Ban Date": "Not Available",
+                        "Banned By": "Unknown",
+                        "Banned Until": "Forever",
+                        "Reason": "Unknown"
+                    }
 
         self.finish({"result": {"success": True, "message": None, "results": result}})
--- MCPanel/Handlers/Servers/Server/Ajax/GetOperators.py
+++ MCPanel/Handlers/Servers/Server/Ajax/GetOperators.py
@@ -3,6 +3,8 @@ __author__ = 'brayden'
 from tornado.web import asynchronous
 from tornado.web import authenticated
 from Base import BaseServerAjaxHandler
+import os
+import json
 
 
 class GetOperatorsHandler(BaseServerAjaxHandler):
@@ -11,10 +13,16 @@ class GetOperatorsHandler(BaseServerAjaxHandler):
     def post(self, server_id):
         ops = []
 
-        with open('%s/%s%s/ops.txt' % (self.application.config.get('minecraft', 'home'), self.application.process_prefix, server_id)) as f:
-            for line in f.readlines():
-                if line.startswith('#'): continue
+        if os.path.exists('%s/%s%s/ops.json' % (self.application.config.get('minecraft', 'home'), self.application.process_prefix, server_id)):
+            with open('%s/%s%s/ops.json' % (self.application.config.get('minecraft', 'home'), self.application.process_prefix, server_id), 'r') as f:
+                data = json.load(f)
+            for op in data:
+                ops.append(op['name'])
+        else:
+            with open('%s/%s%s/ops.txt' % (self.application.config.get('minecraft', 'home'), self.application.process_prefix, server_id)) as f:
+                for line in f.readlines():
+                    if line.startswith('#'): continue
 
-                ops.append(line.strip())
+                    ops.append(line.strip())
 
         self.finish({"result": {"success": True, "message": None, "ops": ops}})
